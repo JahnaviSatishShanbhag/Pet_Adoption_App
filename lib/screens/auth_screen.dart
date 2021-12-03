8cec4378dc2ef7dbc9929d16b55df2044b3329f5
harshitha <harshiikushii811@gmail.com>
2021-12-03 20:20:36 +0530

    homepage

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../widgets/auth_form.dart';
 
 class AuthScreen extends StatefulWidget {
-
   @override
   _AuthScreenState createState() => _AuthScreenState();
 }
@@ -12,8 +11,33 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(79,121,66,0.6),
-      body:AuthForm(),
+      backgroundColor: const Color.fromRGBO(79, 121, 66, 0.6),
+      body: AuthForm(),
+    );
+  }
+}
+
+class homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            );
+          }),
+        ),
+      ),
     );
   }
 }
