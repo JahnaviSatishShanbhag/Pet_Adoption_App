0e3052b9f5a9ac06707aa5614767254a10169651
JahnaviSatishShanbhag <71307702+JahnaviSatishShanbhag@users.noreply.github.com>
2021-12-04 07:31:55 +0530

    Update auth_screen.dart

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -16,28 +16,3 @@ class _AuthScreenState extends State<AuthScreen> {
     );
   }
 }
-
-class homepage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: GridView.count(
-          // Create a grid with 2 columns. If you change the scrollDirection to
-          // horizontal, this produces 2 rows.
-          crossAxisCount: 2,
-          // Generate 100 widgets that display their index in the List.
-          children: List.generate(100, (index) {
-            return Center(
-              child: Text(
-                'Item $index',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            );
-          }),
-        ),
-      ),
-    );
-  }
-}
