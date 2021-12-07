9f626c39010ff685f6a912020f5602cf271b0405
harshitha <harshiikushii811@gmail.com>
2021-12-07 20:46:22 +0530

    commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,109 +7,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
       home: Scaffold(
         appBar: AppBar(
-          // title: const Text("Home"),
-          leading: IconButton(
-            icon: const Icon(
-              Icons.menu,
-            ),
-            onPressed: () {},
-          ),
           backgroundColor: Colors.brown,
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.account_circle_sharp),
-              onPressed: () {},
-=======
-        home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.brown,
-        title: const Text('Pet Adoption'),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.brown,
-              ),
-              child: Text(
-                'Pet Adoption',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.message),
-              title: Text('Categories'),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Create an Account'),
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Log Out'),
-            ),
-          ],
+          title: const Text('Pet Adoption'),
         ),
-      ),
-      backgroundColor: Theme.of(context).primaryColor,
-      body: CustomScrollView(
-        primary: false,
-        slivers: <Widget>[
-          SliverPadding(
-            padding: const EdgeInsets.all(40),
-            sliver: SliverGrid.count(
-              crossAxisSpacing: 30,
-              mainAxisSpacing: 50,
-              crossAxisCount: 2,
-              children: <Widget>[
-                Container(
-                  child: const Image(
-                      image: AssetImage('assets/pup.jpg'),
-                      height: 150,
-                      fit: BoxFit.fill),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.brown,
                 ),
-                Container(
-                  child: const Image(
-                      image: AssetImage('assets/catto.jpg'),
-                      height: 150,
-                      fit: BoxFit.fill),
+                child: Text(
+                  'Pet Adoption',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
                 ),
-                Container(
-                  child: const Image(
-                      image: AssetImage('assets/birds.jpg'),
-                      height: 150,
-                      fit: BoxFit.fill),
-                ),
-                Container(
-                  child: const Image(
-                      image: AssetImage('assets/rabbit.jpg'),
-                      height: 150,
-                      fit: BoxFit.fill),
-                ),
-                Container(
-                  child: const Image(
-                      image: AssetImage('assets/fish.jpg'),
-                      height: 150,
-                      fit: BoxFit.fill),
-                ),
-                Container(
-                  child: const Image(
-                      image: AssetImage('assets/pup.jpg'),
-                      height: 150,
-                      fit: BoxFit.fill),
-                ),
-              ],
->>>>>>> 9810e15cd24f75d13672cceb7b7ed1546a6c523f
-            ),
-          ],
+              ),
+              ListTile(
+                leading: Icon(Icons.message),
+                title: Text('Categories'),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Create an Account'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Log Out'),
+              ),
+            ],
+          ),
         ),
         backgroundColor: Theme.of(context).primaryColor,
         body: GridView(
@@ -130,10 +62,6 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-<<<<<<< HEAD
     );
-=======
-    ));
->>>>>>> 9810e15cd24f75d13672cceb7b7ed1546a6c523f
   }
 }
